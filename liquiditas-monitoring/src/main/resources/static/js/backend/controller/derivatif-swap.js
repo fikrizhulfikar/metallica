5bd872cf478d0bc5f513c66d08e3175357819046
Ardika Rommy Sanjaya <ardikars@gmail.com>
2018-07-01 09:20:53 -0700

    Sorting

--- liquiditas-monitoring/src/main/resources/static/js/backend/controller/derivatif-swap.js
+++ liquiditas-monitoring/src/main/resources/static/js/backend/controller/derivatif-swap.js
@@ -558,7 +558,7 @@ function initDataTable(pTglAwal,pTglAkhir,pBank,pCurrency,pTenor){
             { width: 205, targets: 17 },
             { width: 90, targets: 18 },
             {
-                "bSortable": false,
+                "bSortable": true,
                 "aTargets": [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
             }
             ,
