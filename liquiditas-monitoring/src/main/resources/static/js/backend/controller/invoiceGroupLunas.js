46736de6aa6d52c74e95f856f59992046d40a46a
Fikri Zhulfikar <fikri.zhulfikar@iconpln.co.id>
2020-07-14 13:48:35 +0700

    Modify button class

--- liquiditas-monitoring/src/main/resources/static/js/backend/controller/invoiceGroupLunas.js
+++ liquiditas-monitoring/src/main/resources/static/js/backend/controller/invoiceGroupLunas.js
@@ -442,12 +442,12 @@ function initDataTable(pTglAwal, pTglAkhir, pBank, pCaraBayar, pCurr) {
                         else if(newRoleUser[0] == "ROLE_ADMIN" ){
                             ret_value = ret_value +
                                 '<div class="btn-group">' +
-                                '<button style="width: 15px !important;" class="btn-duplicate-data btn-sm btn-primary" title="Duplicate Data" onclick="duplicate_data(\'' + full.ID_VALAS + '\')"><i class="fa fa-clone"></i></button>'+
-                                '<button style="width: 15px !important;" class="btn-update-data btn-sm btn-success" title="Upload" onclick="upload_file(\'' + full.ID_VALAS + '\')"><i class="fa fa-upload"></i></button>' +
-                                '<button style="width: 15px !important;" class="btn-delete-data btn-sm btn-danger" title="Delete" onclick="delete_data(\'' + full.ID_VALAS + '\')"><i class="fa fa-close"></i></button>' +
+                                '<button style="width: 15px !important; margin-left: 5px;" class="btn btn-duplicate-data btn-sm btn-primary" title="Duplicate Data" onclick="duplicate_data(\'' + full.ID_VALAS + '\')"><i class="fa fa-clone"></i></button>'+
+                                '<button style="width: 15px !important; margin-left: 5px;" class="btn btn-update-data btn-sm btn-success" title="Upload" onclick="upload_file(\'' + full.ID_VALAS + '\')"><i class="fa fa-upload"></i></button>' +
+                                '<button style="width: 15px !important;" class="btn btn-delete-data btn-sm btn-danger" title="Delete" onclick="delete_data(\'' + full.ID_VALAS + '\')"><i class="fa fa-close"></i></button>' +
                                 '</div>';
                         }else {
-                            ret_value = '<div class="btn-group"><button style="width: 15px !important;" class="btn-edit-data btn-sm btn-success" title="Detail" onclick="getDetails(\'' +full.ID_GROUP+'\')"><i class="fa fa-info-circle"></i></button>'+
+                            ret_value = '<div class="btn-group"><button style="width: 15px !important;" class="btn btn-edit-data btn-sm btn-success" title="Detail" onclick="getDetails(\'' +full.ID_GROUP+'\')"><i class="fa fa-info-circle"></i></button>'+
                                 '</div>';
                         }
                         return ret_value;
