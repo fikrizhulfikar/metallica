daa99fa4c23e1350dbc7688d2a8d7a67e373bc97
Fikri Zhulfikar <fikri.zhulfikar@iconpln.co.id>
2020-08-18 12:23:52 +0700

    Fix error, menambahkan fungsi replaceAll

--- liquiditas-monitoring/src/main/resources/static/js/backend/controller/rekapInvoiceSiapBayar.js
+++ liquiditas-monitoring/src/main/resources/static/js/backend/controller/rekapInvoiceSiapBayar.js
@@ -2311,7 +2311,7 @@ function exportXls() {
     if (srcTglAkhir != "") {
         tglAkhir = srcTglAkhir
     }
-    window.open(baseUrl + "api_operator/invoice_siap_bayar/xls/" + tglAwal + "/" + tglAkhir + "/" + $("#cmb_currecny").val() + "/" + $("#cmb_cara_pembayaran").val() + "/" + $("#cmb_bank").val() + "/" +null+ "/" +null);
+    window.open(baseUrl + "api_operator/invoice_siap_bayar/xls/" + tglAwal.replaceAll("/","-") + "/" + tglAkhir.replaceAll("/","-") + "/" + $("#cmb_currecny").val() + "/" + $("#cmb_cara_pembayaran").val() + "/" + $("#cmb_bank").val() + "/" +null+ "/" +null);
 }
 
 function getTotalTagihan() {
