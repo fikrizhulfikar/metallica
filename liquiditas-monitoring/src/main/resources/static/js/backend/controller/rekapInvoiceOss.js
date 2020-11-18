2286c7b8061e1cd7f6d9fa9d7d77a85790b3b59e
Fikri Zhulfikar <fikri.zhulfikar@iconpln.co.id>
2020-11-18 14:16:25 +0700

    Fix bug, radio button tetap disable setelah edit data

--- liquiditas-monitoring/src/main/resources/static/js/backend/controller/rekapInvoiceOss.js
+++ liquiditas-monitoring/src/main/resources/static/js/backend/controller/rekapInvoiceOss.js
@@ -173,6 +173,8 @@ function duplicate_data(id) {
             $("#pSpread").val(res[0].SPREAD);
             $("#pJenisTagihan").val(res[0].JENIS_TAGIHAN.toLowerCase());
             $('#pTglJatuhTempo').prop('disabled', false);
+            $("#hrpayableradio").attr('disabled',false);
+            $("#apinvoiceradio").attr('disabled',false);
             if(newRoleUser[0].replace(" ", "")== "ROLE_OSS"){
                 $('#pTglJatuhTempo').prop('disabled', true);
             }
